b11c3c63c23f2dad23621a16d334113dbba44fc1
Rezuanul Islam Fahim <rifahim98@gmail.com>
2026-01-16 00:04:30 +0600

    feat: add layout template for application with basic HTML structure

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Livewire\ArticleSearch;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
 })->name('home');
+
+Route::get('/search', ArticleSearch::class);
