287c096a381bdccced50661a9e4957615dfbebf2
Rezuanul Islam Fahim <rifahim98@gmail.com>
2026-02-27 01:40:52 +0600

    feat: add PublishedCount component and integrate it into article list view

--- resources/views/livewire/article-list.blade.php
+++ resources/views/livewire/article-list.blade.php
@@ -1,17 +1,20 @@
-<div class="">
-    <a class="bg-blue-600 px-4 py-3 rounded-md hover:bg-blue-700"
-        href="/dashboard/articles/create">
-        Create New Article
-    </a>
+<div class="max-w-2xl mx-auto mt-5">
+    <div class="flex flex-row justify-between items-center">
+        <a class="bg-blue-600 px-4 py-3 rounded-md hover:bg-blue-700"
+            href="/dashboard/articles/create">
+            Create New Article
+        </a>
+        <livewire:published-count />
+    </div>
 
-    <table class="mx-auto mt-6 border border-gray-300">
-        <thead class="border-b border-gray-300">
+    <table class="mt-6 w-full">
+        <thead class="bg-gray-700">
             <th class="p-3">Title</th>
             <th class="p-3"></th>
         </thead>
-        <tbody>
+        <tbody class="bg-gray-800">
             @foreach ($articles as $article)
-                <tr>
+                <tr class="border-b border-gray-700">
                     <td class="p-4" wire:key={{ $article->id }}>
                         {{ $article->title }}</td>
                     <td class="p-4 space-x-2">
